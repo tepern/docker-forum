d3a3621797dc73dc8092ab385e72161a73fd881c
Тепер Наталья <n.teper@itgroup.local>
2021-07-01 18:22:11 +0400

    Laravel-admin, seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(AdminUserRolePermissionSeeder::class);
+        $this->call(AdminMenuSeeder::class);
     }
 }

15a2af626ecba0adf2d31333f0e1bfd05166d780
Тепер Наталья <n.teper@itgroup.local>
2021-11-22 13:30:22 +0400

    User seeder data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(AdminUserRolePermissionSeeder::class);
         $this->call(AdminMenuSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'author',
+                'email' => 'author1@test.ru',
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'Автор',
+                'email' => 'author2@test.ru',
+                'password' => bcrypt('123123'),
+            ],
+        ];
+
+        DB::table('users')->insert($data);
+    }
+}
